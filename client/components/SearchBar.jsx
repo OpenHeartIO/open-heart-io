700281d925fdc156b792ed3cfcfb9f572b341465
Kenneth Trang <trang.kenneth@gmail.com>
2020-01-01 21:00:28 -0800

    Add Searchbar and get request functionality

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const SearchBar = () => {
-  
-}
+  const [searchLocation, setSearchLocation] = useState(null);
+
+  return (
+    <>
+      <input 
+        id="searchbar" 
+        type="search"
+        onChange={(e) => setSearchLocation(e.target.value)}
+      />
+      <input 
+        type="submit" 
+        value="Search"
+        onClick={() => {
+          axios.get('/search', {searchLocation})
+            .then(response => {
+              // If location exists in db, render Location Overview page
+
+              // If location doesn't exist, render Add New Location page
+              
+            })
+            .catch(err => console.log(err))
+        }}
+      />
+    </>
+  );
+};
 
 export default SearchBar;
