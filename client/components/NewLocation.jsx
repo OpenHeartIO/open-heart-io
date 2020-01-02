a0241bf3409f7a6218fe23714e2b942e735fff04
Kenneth Trang <trang.kenneth@gmail.com>
2020-01-01 18:59:32 -0800

    Create 'Add New Location' page container & component

---
+++ client/components/NewLocation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewLocation = () => {
+  return (
+    <div id="new-location-input">
+
+    </div>
+  );
+};
+
+export default NewLocation;
---
+++ client/containers/NewLocationContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewLocation from '../components/NewLocation.jsx';
+
+const NewLocationContainer = () => {
+  return (
+    <div id="new-location-container">
+      <NewLocation/>
+    </div>
+  );
+};
+
+export default NewLocationContainer;
