15b5750ce9a3fcc745d0e5b1639ff93f1bf64771
bmwoolf <bradleymwoolf@gmail.com>
2020-01-01 21:12:20 -0800

    added dataController

---
+++ server/controllers/dataController.js
@@ -0,0 +1,12 @@
+const db = require('../models/databaseModel');
+const procedure = {};
+//Address table: create table Address (_id SERIAL, address VARCHAR(255), country CHAR(3), city VARCHAR(255), state CHAR(2), postal VARCHAR(5), PRIMARY KEY(_id))
+//Operations table: create table Operations (_id serial, data date, insurance_provider varchar(255), pre_insurance_cost int, out_of_pocket_cost int)
+
+db.query();
+
+procedure.getName = (req, res, next) => {
+    //location name === name that is sent
+    let name = req.body.name;
+    
+}
