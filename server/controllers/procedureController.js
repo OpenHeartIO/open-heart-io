aadebfa9f2bb05596c55c480025ebd48ce60a78f
Nobuhide Ajito <najito@Emis-MacBook.local>
2020-01-02 00:21:06 -0800

    controller update

--- server/controllers/procedureController.js
+++ server/controllers/procedureController.js
@@ -20,10 +20,45 @@ procedure.getName = (req, res, next) => {
 
     db.query(query, [location])
     .then((info) => {
-        res.locals.info = info.rows[0];
+        res.locals.info = info.rows;
         next()})
     .catch((err) => next(err))
 }
+
+// procedure.nameParse = (req, res, next) => {
+//     const getName = res.locals.info
+//     const parsed = {};
+//     parsed.procedures = {};
+//     for (let i = 0; i < getName.length; i ++) {
+//         let date = "";
+//         for (let j = 0; j < 11; j ++) {
+//             date += getName[i].date[j]
+//         }
+//         if (Object.hasOwnProperty(parsed.procedures[getName[i].procedure])) {
+//             const current = parsed.procedures[getName[i].procedure]
+//             current.range = res.locals.quickMaths.range
+//             current.avg = res.locals.avg
+//             current.clicked = false;
+//             current.entries.push({
+
+//             })
+//         } else {
+//             parsed.procedures[getName[i].procedure] = {};
+//             const current = parsed.procedures[getName[i].procedure]
+//             current.range = res.locals.quickMaths.range
+//             current.avg = res.locals.quickMaths.avg
+//             current.clicked = false;
+//             current.entries = [{
+//                 "date": date,
+//                 "insurance": insurance,
+//                 "preInsurancecCost": pre_insurance,
+//                 "outOfPocketCost": out_of_pocket,
+//             }]
+//         }
+        
+//     }
+
+// }
 
 
 module.exports = procedure
