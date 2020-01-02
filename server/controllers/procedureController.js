aa383dd1ae379eb1d06b1b18ac281147e3f82502
Nobuhide Ajito <najito@Emis-MacBook.local>
2020-01-01 22:46:30 -0800

    procedure controller added location, cleaned up spacing in server, added nodemon to script

--- server/controllers/procedureController.js
+++ server/controllers/procedureController.js
@@ -2,12 +2,12 @@ const db = require('../models/databaseModel.js')
 const procedure = {}
 
 procedure.createEntry = (req, res, next) => {
-    const {procedure, date, insurance, preinsuranceCost, oopCost} = req.body
+    const {location, procedure, date, insurance, preinsuranceCost, oopCost} = req.body
 
-    const query = `INSERT INTO megatable (procedure, date, insurance, pre_insurance, out_of_pocket)
-    values($1, $2, $3, $4, $5)`
+    const query = `INSERT INTO megatable (location, procedure, date, insurance, pre_insurance, out_of_pocket)
+    values($1, $2, $3, $4, $5, $6)`
 
-    db.query(query, [procedure, date, insurance, preinsuranceCost, oopCost])
+    db.query(query, [location, procedure, date, insurance, preinsuranceCost, oopCost])
     .then(()=> next())
     .catch((err)=> next(err))
 }
--- server/server.js
+++ server/server.js
@@ -10,33 +10,33 @@ app.use(express.json());
 
 // Serve html
 app.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, '../index.html'));
+  res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
 
 // Serve build
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
 //get location information
-app.get('/search', procedure.getName, (req, res) => {
-    res.sendStatus(200)
-})
+// app.get('/search', procedure.getName, (req, res) => {
+//     res.sendStatus(200)
+// })
 
 //add information to location
 app.post('/create', procedure.createEntry, (req, res) => {
-    res.sendStatus(200)
+  res.sendStatus(200)
 })
 
 //404 handler
 app.use('*', (req, res) => {
-    res.sendStatus(404);
-  });
+  res.sendStatus(404);
+});
   
 //global error handler
 app.use((err, req, res, next) => {
-    console.log(err);
-    res.sendStatus(500);
+  console.log(err);
+  res.sendStatus(500);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`);
+  console.log(`Server listening on port: ${PORT}`);
 });
