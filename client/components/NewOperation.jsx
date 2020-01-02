6c281344954a942598e2690fc48487842ec2625c
Kenneth Trang <trang.kenneth@gmail.com>
2020-01-01 19:49:52 -0800

    Create 'Add New Operation' page container & component

---
+++ client/components/NewOperation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewOperation = () => {
+  return (
+    <div id="new-operation-form">
+      
+    </div>
+  );
+};
+
+export default NewOperation;
---
+++ client/containers/NewOperationContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewOperation from '../components/NewOperation.jsx'
+
+const NewOperationContainer = () => {
+  return (
+    <div id="new-operation-container">
+      <NewOperation/>
+    </div>
+  );
+};
+
+export default NewOperationContainer;
