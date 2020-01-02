dcd45faf8a99175a8c9fd683fb77e47064c7d9b6
aburkett1 <austin_osten@hotmail.com>
2020-01-01 17:46:54 -0800

    Added base views for App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,59 @@
 import React from  "react";
 
-const App = () => {  
-    return (
-        <h1>hello</h1>
+import HeaderContainer from './containers/HeaderContainer.jsx';
+import MainContainer from './containers/MainContainter.jsx';
+import OperationsContainer from './containers/OperationContainer.jsx';
+import SearchBar from './components/SearchBar.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // Data from Backend
+      locationInfo: null,
+      // Booleans for conditional loading
+      location: false,
+      addNewLocation: false,
+      addNewOperation: false,
+
+    }
+    
+  }
+
+  updateLocation(search) {
+    fetch(`/location/${search}`)
+      .then(data => data.json())
+      .then(result => this.setState({locationInfo: result}))
+  }
+
+  render() {
+    let view;
+    if (!location && !addNewLocation) {
+      view = (
+        <div id="app">
+          <HeaderContainer/>
+          <SearchBar/>
+        </div>
       );
+    } else if (location) {
+      view = (
+        <div id="app">
+          <HeaderContainer/>
+          <MainContainer/>
+        </div>
+      );
+    } else if (addNewLocation) {
+      view = (
+        <div id="app">
+          <HeaderContainer/>
+          <OperationsContainer/>
+        </div>
+      );
+    }
+    return (
+      { view }
+    )
+  }
 }
  
 export default App;
